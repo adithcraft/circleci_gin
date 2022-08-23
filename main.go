2deba3f9ade9a90d1f4ee29a3a361b7de79e26fc
adith-laptop\adithcraft <adith.prasetya@gmail.com>
2022-08-23 13:42:43 +0700

    try to deploy to aws ec2

--- main.go
+++ main.go
@@ -1,5 +1,74 @@
+// Recipes API
+//
+// This is a sample recipes API. You can find out more about the API at https://github.com/PacktPublishing/Building-Distributed-Applications-in-Gin.
+//
+//	Schemes: http
+//  Host: localhost:8080
+//	BasePath: /
+//	Version: 1.0.0
+//	Contact: Mohamed Labouardy <mohamed@labouardy.com> https://labouardy.com
+//
+//	Consumes:
+//	- application/json
+//
+//	Produces:
+//	- application/json
+// swagger:meta
 package main
 
-func main() {
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
 
+	"github.com/adithcraft/circleci_gin/handlers"
+	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+var (
+	recipesHandler *handlers.RecipesHandler
+)
+
+func init() {
+	ctx := context.Background()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("MONGO_URI")))
+	if err != nil {
+		log.Fatalf("Failed to connect mongodb %v", err)
+	}
+	if err = client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		log.Fatalf("Failed to ping mongodb %v", err)
+	}
+	log.Println("Connected to mongodb ")
+	collection := client.Database(os.Getenv("MONGO_DATABASE")).Collection("recipes")
+
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     os.Getenv("REDIS_URI"),
+		Password: "",
+		DB:       0,
+	})
+
+	status := redisClient.Ping()
+	log.Printf("redis status %v", status)
+
+	recipesHandler = handlers.NewRecipesHandler(ctx, collection, redisClient)
+}
+
+func VersionHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"version": os.Getenv("API_VERSION")})
+}
+
+func main() {
+	router := gin.Default()
+
+	router.POST("/recipes", recipesHandler.NewRecipeHandler)
+	router.GET("/recipes", recipesHandler.ListRecipesHandler)
+	router.PUT("/recipes/:id", recipesHandler.UpdateRecipeHandler)
+	router.GET("/version", VersionHandler)
+
+	router.Run()
 }
